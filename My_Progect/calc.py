fd99d432523057ce3f3e855b7d7b89c305021111
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2022-05-18 23:18:53 +0600

    Добавил пару новых работ

--- My_Progect/calc.py
+++ My_Progect/calc.py
@@ -1,16 +1,39 @@
-a = int(input("Введите число: "))
-b = str(input("Введите знак(+,-,/,*) : "))
-c = int(input("Введите число : "))
+import math
 
-def calc(a,b,c):
-    if b == "+":
-        print(a+c)
-    elif b == "-":
-        print(a-c)
-    elif b == "/":
-        print(a/c)
-    elif b == "*":
-        print(a*c)
+a = float(input("Введите число: "))
+b = str(input("Введите знак(+,-,/,*,**,%,//,sqrt) : "))
+if b == "sqrt":
+    print(math.sqrt(a))
+else:
+    c = float(input("Введите число : "))
+
+    def calc(a,b,c):
+
+        if b == "+":
+            print(a+b+c)
+        elif b == "-":
+            print(a-c)
+        elif b == "/":
+            if c == 0:
+                print("На ноль делить нельзя")
+            else:
+                print("Результат деления:")
+                print(a/c)
+        elif b == "*":
+            print("Результат умножения:")
+            print(a*c)
+        elif b == "**":
+            print("Результат возведения в степень:")
+            print(a**c)
+        elif b == "%":
+            print("Остаток от деления:")
+            print(a % c)
+        elif b == "//":
+            print("Результат без остатка:")
+            print(a // c)
+
+        calc(a,b,c)
 
 
-calc(a,b,c)
+
+# .split (',')
---
+++ My_Progect/calc2.py
@@ -0,0 +1,96 @@
+import math
+a = ""
+d = ""
+b = ""
+input = str(input("Введите выражение, знаки(+,-,/,*,**,%,//,√): "))
+# b = str(input("Введите знак(+,-,/,*,**,%,//,sqrt) : "))
+
+if input.find("+") == 1:
+    b = "+"
+    input = input.split("+")
+    d = input[1]
+    a = input[0]
+
+elif input.find("-") == 1:
+    b = "-"
+    input = input.split("-")
+    a = input[0]
+    d = input[1]
+
+elif input.find("//") == 1:
+    b = "//"
+    input = input.split("//")
+    a = input[0]
+    d = input[1]
+    print(input)
+
+elif input.find("/") == 1:
+    b = "/"
+    input = input.split("/")
+    a = input[0]
+    d = input[1]
+
+elif input.find("**") == 1:
+    b = "**"
+    input = input.split("**")
+    a = input[0]
+    d = input[1]
+
+
+elif input.find("*") == 1:
+    b = "*"
+    input = input.split("*")
+    a = input[0]
+    d = input[1]
+
+
+elif input.find("%") == 1:
+    b = "%"
+    input = input.split("%")
+    a = input[0]
+    d = input[1]
+
+
+#
+# elif input.find("√") == 1:
+#     b = "sqrt"
+#     input = input.split("sqrt")
+#     a = float(input[0])
+#     d = "1"
+#
+
+
+
+# print(a)
+# print(d)
+a = float(a)
+d = float(d)
+if b == "sqrt":
+    print(math.sqrt(a))
+else:
+    def calc(a, b, d):
+        if b == "+":
+            print(a + d)
+        elif b == "-":
+            print(a - d)
+        elif b == "/":
+            if d == 0:
+                print("На ноль делить нельзя")
+            else:
+                print("Результат деления:")
+                print(a / d)
+        elif b == "*":
+            print("Результат умножения:")
+            print(a * d)
+        elif b == "**":
+            print("Результат возведения в степень:")
+            print(a ** d)
+        elif b == "%":
+            print("Остаток от деления:")
+            print(a % d)
+        elif b == "//":
+            print("Результат без остатка:")
+            print(a // d)
+    calc(a, b, d)
+
+# .split (',')
